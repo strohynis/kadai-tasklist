4ae96834364bfa844903510684b640223c403449
yy07266 <y9yuron@gmail.com>
2021-08-12 08:07:55 +0000

    user login_fix3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,9 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :require_user_logged_in, only: [:index, :show, :edit]
+  before_action :correct_user, only: [:show, :edit, :destroy]
   
   def index
-    if logged_in?
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
   end
   
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   root to: 'tasks#index'
   
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'toppages/index'
-
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
+
+
   resources :tasks
   resources :users, only: [:index, :show, :create]
 end
